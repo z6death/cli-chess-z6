c38b46121792b014a9eec2325528fbbd0ddc9a84
z6death <tnz426.z6@gmail.com>
2025-06-22 18:36:29 +0600

    Initial commit

---
+++ custom_style.py
@@ -0,0 +1,30 @@
+{
+    "light-square": "bg:green",
+    "light-square.light-piece": "fg:white",
+    "light-square.dark-piece": "fg:lime",
+
+    "dark-square": "bg:black",
+    "dark-square.light-piece": "fg:white",
+    "dark-square.dark-piece": "fg:lime",
+
+    "last-move": "bg:mediumseagreen",
+    "last-move.light-piece": "fg:white",
+    "last-move.dark-piece": "fg:black",
+
+    "in-check": "bg:orchid",
+    "in-check.light-piece": "fg:white",
+    "in-check.dark-piece": "fg:lime",
+
+    "menu": "bg:black",
+    "menu.category-title": "fg:black bg:limegreen",
+    "menu.option": "fg:white",
+    "menu.multi-value": "fg:seagreen",
+    "focused-selected": "fg:black bg:springgreen",
+    "unfocused-selected": "fg:white bg:seagreen",
+    "menu.multi-value focused-selected": "fg:black",
+    "menu.multi-value unfocused-selected": "fg:turquoise bg:black",
+
+    "function-bar.key": "fg:white",
+    "function-bar.label": "fg:black bg:springgreen",
+    "function-bar.spacer": " ",
+    }
